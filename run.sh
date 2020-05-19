29793c387b279e333da8fe5fb8603622594ebf9f
artemkulish123 <artemkulish123@gmail.com>
2020-05-19 16:53:43 +0300

    Build type - war

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-./SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=payment
-
-java -jar *.jar
+bash /opt/tomcat/bin/catalina.sh start
+cd /home && ./SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=payment &
+tail -f /dev/null
--- src/main/java/com/softserve/paymentservice/KickScooterPaymentServiceApplication.java
+++ src/main/java/com/softserve/paymentservice/KickScooterPaymentServiceApplication.java
@@ -3,10 +3,17 @@ package com.softserve.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @EnableSwagger2
 @SpringBootApplication
-public class KickScooterPaymentServiceApplication {
+public class KickScooterPaymentServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KickScooterPaymentServiceApplication.class);
+    }    
     public static void main(String[] args) {
         SpringApplication.run(KickScooterPaymentServiceApplication.class, args);
     }
